fe7e1fda41f61a5f5548687a2e88fb4dd067ebd6
JL-OLEMAR <joseluis19963@gmail.com>
2021-03-27 21:24:45 -0500

    diseño loginPage and registerPage

--- src/router/AuthRouter.js
+++ src/router/AuthRouter.js
@@ -3,14 +3,22 @@ import { Redirect, Route, Switch } from 'react-router'
 
 import { LoginPage } from '../pages/LoginPage'
 import { RegisterPage } from '../pages/RegisterPage'
+import '../css/login-register.css'
 
 export const AuthRouter = () => {
   return (
-    <Switch>
-      <Route exact path="/auth/login" component={LoginPage} />
-      <Route exact path="/auth/register" component={RegisterPage} />
 
-      <Redirect to="/auth/login" />
-    </Switch>
+    <div className="limiter">
+      <div className="container-login100">
+        <div className="wrap-login100 p-t-50 p-b-90">
+          <Switch>
+            <Route exact path="/auth/login" component={LoginPage} />
+            <Route exact path="/auth/register" component={RegisterPage} />
+
+            <Redirect to="/auth/login" />
+          </Switch>
+        </div>
+      </div>
+    </div>
   )
 }
