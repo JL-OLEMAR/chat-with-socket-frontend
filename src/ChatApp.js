14d60eefca166a7c652ac14b63f2288e993121a7
JL-OLEMAR <joseluis19963@gmail.com>
2021-03-31 10:58:35 -0500

    authentication

--- src/ChatApp.js
+++ src/ChatApp.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import { AuthProvider } from './auth/AuthContext'
 import { AppRouter } from './router/AppRouter'
 
 export const ChatApp = () => {
   return (
-    <div>
+    <AuthProvider>
       <AppRouter />
-    </div>
+    </AuthProvider>
   )
 }
--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -1,25 +1,83 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
 import { Link } from 'react-router-dom'
 
+import Swal from 'sweetalert2'
+import { AuthContext } from '../auth/AuthContext'
+
 export const RegisterPage = () => {
+  const { register } = useContext(AuthContext)
+
+  const [form, setForm] = useState({
+    name: 'Fernando Herrera',
+    email: 'test5@test.com',
+    password: '123456'
+  })
+
+  const onChange = ({ target }) => {
+    const { name, value } = target
+    setForm({
+      ...form,
+      [name]: value
+    })
+  }
+
+  const onSubmit = async (ev) => {
+    ev.preventDefault()
+
+    const { name, email, password } = form
+    const ok = await register(name, email, password)
+
+    if (!ok) {
+      Swal.fire('Error', 'Completa todos los campos correctamente!', 'error')
+    }
+  }
+
+  const todoOk = () => {
+    return !!((form.name.length > 0 && form.email.length > 0 && form.password.length > 0))
+  }
+
   return (
-    <form className="login100-form validate-form flex-sb flex-w">
+    <form
+      className="login100-form validate-form flex-sb flex-w"
+      onSubmit={onSubmit}
+    >
       <span className="login100-form-title mb-3">
         Chat - Registro
       </span>
 
       <div className="wrap-input100 validate-input mb-3">
-        <input className="input100" type="text" name="name" placeholder="Nombre" />
+        <input
+          className="input100"
+          type="text"
+          name="name"
+          placeholder="Nombre"
+          value={form.name}
+          onChange={onChange}
+        />
         <span className="focus-input100"></span>
       </div>
 
       <div className="wrap-input100 validate-input mb-3">
-        <input className="input100" type="email" name="email" placeholder="Email" />
+        <input
+          className="input100"
+          type="email"
+          name="email"
+          placeholder="Email"
+          value={form.email}
+          onChange={onChange}
+        />
         <span className="focus-input100"></span>
       </div>
 
       <div className="wrap-input100 validate-input mb-3">
-        <input className="input100" type="password" name="password" placeholder="Password" />
+        <input
+          className="input100"
+          type="password"
+          name="password"
+          placeholder="Password"
+          value={form.password}
+          onChange={onChange}
+        />
         <span className="focus-input100"></span>
       </div>
 
@@ -32,7 +90,11 @@ export const RegisterPage = () => {
       </div>
 
       <div className="container-login100-form-btn m-t-17">
-        <button className="login100-form-btn">
+        <button
+          type="submit"
+          className="login100-form-btn"
+          disabled={!todoOk()}
+        >
           Crear cuenta
         </button>
       </div>
