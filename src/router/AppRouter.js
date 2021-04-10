a7349e380ffdb93eb79171c2ee2ede73e8741745
JL-OLEMAR <joseluis19963@gmail.com>
2021-04-09 19:34:01 -0500

    rutas publicas y privadas

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,9 +1,11 @@
 import React, { useContext, useEffect } from 'react'
-import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Redirect } from 'react-router-dom'
 import { AuthContext } from '../auth/AuthContext'
 
 import { ChatPage } from '../pages/ChatPage'
 import { AuthRouter } from './AuthRouter'
+import { PrivateRoute } from './PrivateRoute'
+import { PublicRoute } from './PublicRoute'
 
 export const AppRouter = () => {
   const { auth, verificaToken } = useContext(AuthContext)
@@ -20,8 +22,9 @@ export const AppRouter = () => {
     <Router>
       <div>
         <Switch>
-          <Route path="/auth" component={AuthRouter} />
-          <Route exact path="/" component={ChatPage} />
+          {/* <Route path="/auth" component={AuthRouter} /> */}
+          <PublicRoute isAuthenticated={auth.logged} path="/auth" component={AuthRouter} />
+          <PrivateRoute isAuthenticated={auth.logged} exact path="/" component={ChatPage} />
 
           <Redirect to="/" />
         </Switch>
---
+++ src/router/PrivateRoute.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Redirect, Route } from 'react-router-dom'
+
+export const PrivateRoute = ({
+  isAuthenticated,
+  component: Component,
+  ...rest
+}) => {
+  return (
+    <Route {...rest}
+      component={(props) => (
+        (isAuthenticated)
+          ? <Component {...props} />
+          : <Redirect to="/auth" />
+      )}
+    />
+  )
+}
